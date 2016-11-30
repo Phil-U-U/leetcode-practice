5760352e9d2c37dab260a766fd0f8f65bf05d667
Phil Cui <phil.h.cui@gmail.com>
2016-11-29 16:38:45 -0800

    Leetcode format

--- insert-interval/insert-interval.py
+++ insert-interval/insert-interval.py
@@ -28,6 +28,37 @@ class Solution( object ):
         return res
 
 
+# Definition for an interval.
+# class Interval(object):
+#     def __init__(self, s=0, e=0):
+#         self.start = s
+#         self.end = e
+
+class Solution_interval_object(object):
+    def insert(self, intervals, newInterval):
+        """
+        :type intervals: List[Interval]
+        :type newInterval: Interval
+        :rtype: List[Interval]
+        """
+        res = []
+
+        for interval in intervals:
+            if newInterval.end < interval.start:
+                res.append( newInterval )
+                newInterval = interval
+            elif newInterval.start > interval.end:
+                res.append( interval )
+            else:
+                newInterval.start = min( interval.start, newInterval.start )
+                newInterval.end = max( interval.end, newInterval.end )
+
+        res.append( newInterval )
+
+        return res
+
+
+
 if __name__ == '__main__':
     solution = Solution()
     intervals = [[1,3],[6,9]]
