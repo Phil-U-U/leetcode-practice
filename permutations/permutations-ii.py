2992320203d9eb7ff145c1678512b06f7b389a4e
Phil Cui <phil.h.cui@gmail.com>
2016-12-02 14:33:46 -0800

    permutations-ii

--- permutations/permutations-ii.py
+++ permutations/permutations-ii.py
@@ -10,6 +10,20 @@ For example,
   [2,1,1]
 ]
 
+VS
+
+[1,2,3] ->
+[
+    [1,2,3],  [1,2,1]
+    [1,3,2],  [1,1,2]
+    [2,1,3],  [2,1,1]
+    [2,3,1],  [2,1,1]
+    [3,1,2],  [1,1,2]
+    [3,2,1],  [1,2,1]
+]
+
+Author: Phil H. Cui
+Date: 12/02/16
 '''
 
 
@@ -21,8 +35,25 @@ class Solution(object):
         :rtype: List[List[int]]
         """
 
-        
+        if len(nums) == 1:
+            return [nums]
+
+        res = []
+        duplicates = set()
+
+        for i, num in enumerate(nums):
+            if num in duplicates:
+                continue
+            else:
+                duplicates.add( num )
+
+                rest = nums[:i] + nums[i+1:]
+
+                for restPermutation in self.permuteUnique( rest ):
+                    res.append( [num] + restPermutation )
+
+        return res
 
 if __name__ == '__main__':
-    nums = [1, 1, 2]
+    nums = [1, 2, 1]
     print Solution().permuteUnique( nums )
