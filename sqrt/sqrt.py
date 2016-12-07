9f4e9f7c3abc2bee9dba3548eb3d49a16d73dc14
Phil Cui <phil.h.cui@gmail.com>
2016-12-07 10:35:49 -0800

    sqrt

--- sqrt/sqrt.py
+++ sqrt/sqrt.py
@@ -10,15 +10,46 @@ Date: 12/06/16
 '''
 
 class Solution(object):
-    def mySqrt(self, x):
+    def __init__( self ):
+        self.episilon = 0.001
+
+    def mySqrt_2(self, x):
         """
         :type x: int
         :rtype: int
         """
 
+        low, high = 0, x/2 + 1
+
+        while low <= high:
+
+            mid = (low + high)/2
+
+            # if abs(mid ** 2 - x) <= self.episilon:
+            #     return mid
+            if mid ** 2 == x:
+                return mid
+
+            elif mid ** 2 > x:
+                high = mid - 1
+            else:
+                low = mid + 1
+
+        return high
 
 
-        pass
+    def mySqrt(self, x):
+
+        low, high = 0, x/2 + 1
+
+        while low <= high:
+            mid = (low + high) / 2.0
+            if abs(mid ** 2 - x) <= self.episilon:
+                return mid
+            elif mid ** 2 > x:
+                high = mid
+            else:
+                low = mid
 
 
 if __name__ == "__main__":
