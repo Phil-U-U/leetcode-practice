e46f7d23eeac86e80cce70625624ae0b6af75c75
Phil Cui <phil.h.cui@gmail.com>
2016-12-08 11:47:57 -0800

    paint-house-ii

--- paint-house/paint-house-i.py
+++ paint-house/paint-house-i.py
@@ -10,6 +10,13 @@ Find the minimum cost to paint all houses.
 
 Note: All costs are positive integers.
 
+
+直到房子i，其最小的涂色开销是直到房子i-1的最小涂色开销，加上房子i本身的涂色开销。
+但是房子i的涂色方式需要根据房子i-1的涂色方式来确定，所以我们对房子i-1要记录涂三种颜色分别不同的开销，
+这样房子i在涂色的时候，我们就知道三种颜色各自的最小开销是多少了。我们在原数组上修改，可以做到不用空间。
+
+
+
 Author: Phil H. Cui
 Date: 12/07/16
 
