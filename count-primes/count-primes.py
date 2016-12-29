85de127039af00c4dffa8d590bd7c48f7ce7436e
Phil Cui <phil.h.cui@gmail.com>
2016-12-29 09:28:51 -0800

    count primes

---
+++ count-primes/count-primes.py
@@ -0,0 +1,21 @@
+'''
+
+Description:
+
+Count the number of prime numbers less than a non-negative number, n.
+
+Author: Phil H. Cui
+Date: 12/29/16
+'''
+class Solution(object):
+    def countPrimes(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+
+
+
+
+if __name__ == "__main__":
+    print Solution().countPrimes(18)
---
+++ longest-palindromic-substring/longest-palindromic-substring.py
@@ -0,0 +1,23 @@
+'''
+Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+
+Input: "babad"; Output: "bab"; Note: "aba" is also a valid answer.
+Input: "cbbd"; Output: "bb"
+
+Author: Phil H. Cui
+Date: 12/26/16
+'''
+
+
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+
+
+if __name__ == "__main__":
+    s1, s2 = "cbbd", "babad"
+
+    print Solution().longestPalindrome(s1)
